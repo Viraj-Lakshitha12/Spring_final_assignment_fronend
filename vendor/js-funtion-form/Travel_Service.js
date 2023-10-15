82ec2c4c0556ca30e214073ab7c079f2b8014c41
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-10-15 20:59:22 +0530

    update content

--- vendor/js-funtion-form/Travel_Service.js
+++ vendor/js-funtion-form/Travel_Service.js
@@ -1,6 +1,6 @@
 $("#payment-button").click(function () {
     var category = $("#package-category option:selected").text();
-    var startDate = $("#Start_Date").val();
+    var sDate = $("#Start_Date").val();
     var endDate = $("#End_Date").val();
     var travelArea = $("#Travel_Area option:selected").text();
     var noOfAdults = $("#No-of-adults").val();
@@ -15,20 +15,10 @@ $("#payment-button").click(function () {
     var userId = $("#user-ids option:selected").text();
     var totalAmount = $("#Amount").val();
 
-    // console.log("package category"+selectedDescription);
-    // console.log("Start Date: " + startDate);
-    // console.log("End Date: " + endDate);
-    // console.log("Travel Area: " + travelArea);
-    // console.log("No of Adults: " + noOfAdults);
-    // console.log("No of Children: " + noOfChildren);
-    // console.log("Total Headcount: " + totalHeadcount);
-    // console.log("With Pets: " + withPets);
-    // console.log("Need Guide: " + needGuide);
-    // console.log("Hotel Fee: " + hotelFee);
-    // console.log("Vehicle Fee: " + vehicleFee);
-    // console.log("Service Charge: " + serviceCharge);
-    // console.log("User ID: " + userId);
-    // console.log("Total Amount: " + totalAmount);
+    var startDate=null;
+
+
+    console.log(startDate);
 
     var formData = {
 
@@ -69,6 +59,51 @@ $("#payment-button").click(function () {
         error: function (error) {
             // Handle any errors
             console.error("POST request failed: " + JSON.stringify(error));
+        }
+    });
+});
+
+
+// loadAll Details
+function formatDate(dateString) {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
+}
+
+$(document).ready(function() {
+    // Make an AJAX request to get all details from your Spring Boot endpoint
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8081/api/v1/customer/getAllData", // Replace with your endpoint URL
+        success: function (data) {
+            const tableBody = $("#table-body");
+
+            if (Array.isArray(data)) {
+                data.forEach(function(item) {
+                    // Create a new row for each item
+                    const row = $("<tr>");
+                    row.append($("<td>").text(item.packageID));
+                    row.append($("<td>").text(formatDate(item.startDate))); // Format the start date
+                    row.append($("<td>").text(formatDate(item.endDate)));   // Format the end date
+                    row.append($("<td>").text(item.travelArea));
+                    row.append($("<td>").text(item.hotelFee));
+                    row.append($("<td>").text(item.vehicleFee));
+                    row.append($("<td>").text(item.serviceCharge));
+                    row.append($("<td>").text(item.userId));
+                    row.append($("<td>").text(item.totalAmount));
+
+                    tableBody.append(row);
+                });
+            } else {
+                // Display an error message if the data is not in the expected array format
+                tableBody.append('<tr><td colspan="9">Data is not in the expected format.</td></tr>');
+            }
+        },
+        error: function (error) {
+            // Display an error message if the AJAX request fails
+            tableBody.append('<tr><td colspan="9">Error loading data. Please try again later.</td></tr>');
+            console.error("Error loading data: " + JSON.stringify(error));
         }
     });
 });
